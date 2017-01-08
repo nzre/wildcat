d761d28f4807c7a065072a9104762147f7f7365f
Henrique Nazaré <henrique.nazare.santos@gmail.com>
2017-01-08 13:19:13 -0200

    Add parsing for symbols.

--- src/Wildcat.hs
+++ src/Wildcat.hs
@@ -1,1 +1,11 @@
-module Wildcat where 
+module Wildcat where
+
+import Control.Monad
+import Text.ParserCombinators.Parsec
+
+data WildcatData =
+  Atom String
+  deriving (Eq, Show)
+
+symbol :: Parser WildcatData
+symbol = liftM Atom $ many1 letter
--- test/WildcatTest.hs
+++ test/WildcatTest.hs
@@ -2,5 +2,19 @@ module WildcatTest where
 
 import qualified Wildcat as W
 
+import Test.Hspec
+import Text.ParserCombinators.Parsec
+
+atom = W.Atom
+
+symbol = parse W.symbol "symbol"
+
 main :: IO ()
-main = return ()
+main = hspec $ do
+  describe "symbol" $ do
+    it "correctly parses a single a-z character as a symbol" $ do
+      symbol "a" `shouldBe` (Right $ atom "a")
+      symbol "z" `shouldBe` (Right $ atom "z")
+    it "correctly parses multiple a-z characters as a symbol" $ do
+      symbol "abc" `shouldBe` (Right $ atom "abc")
+      symbol "xyz" `shouldBe` (Right $ atom "xyz")
