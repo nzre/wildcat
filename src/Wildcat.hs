d97267ce00fb3dd456b4aa418ad83eac0f017661
Henrique Nazaré <henrique.nazare.santos@gmail.com>
2017-01-08 14:07:37 -0200

    Add parsing for ','- and ' '-separated lists.

--- src/Wildcat.hs
+++ src/Wildcat.hs
@@ -4,8 +4,20 @@ import Control.Monad
 import Text.ParserCombinators.Parsec
 
 data WildcatData =
-  Atom String
+  Atom String |
+  List [WildcatData]
   deriving (Eq, Show)
 
+withSurroundingSpaces :: Parser p -> Parser p
+withSurroundingSpaces p = spaces >> p <* spaces
+
+-- Base defs.
 symbol :: Parser WildcatData
 symbol = liftM Atom $ many1 letter
+
+-- Compound defs.
+commaSeparatedList :: Parser WildcatData -> Parser WildcatData
+commaSeparatedList p = liftM List $ sepBy p $ try $ withSurroundingSpaces $ char ',' 
+
+spaceSeparatedList :: Parser WildcatData -> Parser WildcatData
+spaceSeparatedList p = liftM List $ many $ p <* spaces
--- test/WildcatTest.hs
+++ test/WildcatTest.hs
@@ -6,8 +6,14 @@ import Test.Hspec
 import Text.ParserCombinators.Parsec
 
 atom = W.Atom
+list = W.List
+
+atomList = list . map atom
 
 symbol = parse W.symbol "symbol"
+
+commaSeparatedList p = parse (W.commaSeparatedList p) "commaSeparatedList"
+spaceSeparatedList p = parse (W.spaceSeparatedList p) "spaceSeparatedList"
 
 main :: IO ()
 main = hspec $ do
@@ -18,3 +24,21 @@ main = hspec $ do
     it "correctly parses multiple a-z characters as a symbol" $ do
       symbol "abc" `shouldBe` (Right $ atom "abc")
       symbol "xyz" `shouldBe` (Right $ atom "xyz")
+  describe "commaSeparatedList" $ do
+    it "correctly handles an empty list" $ do
+      spaceSeparatedList W.symbol "" `shouldBe` (Right $ list [])
+    it "correctly handles spaces preceding and trailing commas" $ do
+      commaSeparatedList W.symbol "a,b" `shouldBe` (Right $ atomList ["a", "b"])
+      commaSeparatedList W.symbol "a, b" `shouldBe` (Right $ atomList ["a", "b"])
+      commaSeparatedList W.symbol "a ,b" `shouldBe` (Right $ atomList ["a", "b"])
+      commaSeparatedList W.symbol "a , b" `shouldBe` (Right $ atomList ["a", "b"])
+    it "ignores trailing whitespace" $ do
+      commaSeparatedList W.symbol "a, b " `shouldBe` (Right $ atomList ["a", "b"])
+  describe "spaceSeparatedList" $ do
+    it "correctly handles an empty list" $ do
+      spaceSeparatedList W.symbol "" `shouldBe` (Right $ list [])
+    it "correctly parses a space-separated list" $ do
+      spaceSeparatedList W.symbol "a b" `shouldBe` (Right $ atomList ["a", "b"])
+      spaceSeparatedList W.symbol "a  b" `shouldBe` (Right $ atomList ["a", "b"])
+    it "ignores trailing whitespace" $ do
+      spaceSeparatedList W.symbol "a b " `shouldBe` (Right $ atomList ["a", "b"])
